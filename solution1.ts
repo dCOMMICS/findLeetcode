153ba1a659c247d9ef9f4fac7ba59a7b7d65eb12
dCOMMICS <sanwurld@gmail.com>
2023-09-01 13:03:43 +0300

    solution

---
+++ solution1.ts
@@ -0,0 +1,10 @@
+function mergeAlternately(word1: string, word2: string): string {
+    const res = [];
+    const n = Math.max(word1.length, word2.length);
+    for (let i = 0; i < n; i++) {
+        // ignore thise errors//
+        word1[i] && res.push(word1[i]);
+        word2[i] && res.push(word2[i]);
+    }
+    return res.join('');
+}
